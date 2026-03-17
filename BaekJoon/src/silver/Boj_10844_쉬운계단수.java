21ad2196d639eb3e5f59e0a008ecff09c9da7802
dlacowns21 <dlacowns21@naver.com>
2026-03-17 13:23:17 +0900

    [BOJ] 쉬운계단수 / 실버1 / 40분

---
+++ BaekJoon/src/silver/Boj_10844_쉬운계단수.java
@@ -0,0 +1,42 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_10844_쉬운계단수 {
+
+    static int N;
+    static long MOD = 1_000_000_000L;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[N + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+
+                if (j == 0) {
+                    dp[i][0] = dp[i - 1][1] % MOD;
+                } else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8] % MOD;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result += dp[N][i];
+        }
+
+        System.out.println(result % MOD);
+    }
+}
